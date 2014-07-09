932455c5520149194e889bfacfcd9f20743c56b7
Stephen Hughes <Hughes0687@gmail.com>
2014-07-09 11:03:10 -0500

    all tests working

--- src/Events.js
+++ src/Events.js
@@ -8,5 +8,11 @@ var Events = {
       this.events[eventName] = [];
     }
     this.events[eventName].push(callback);
+  },
+
+  trigger: function(eventName) {
+    for(var i = 0, l = this.events[eventName].length; i < l; i++){
+      this.events[eventName][i]();
+    }
   }
 }
